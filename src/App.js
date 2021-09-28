cab6e96aa6ad78912f636ffe7d5d74b8bc7aed5c
pedroffd <pedro.alcarin@gmail.com>
2021-09-28 00:56:36 -0400

    project initial configuration

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+/* function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+} */
+
+export default class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      string: 'Hello Pedro'
+    };
+  }
+  render(){
+    return (
+      <div className='app'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo'/>
+          <p>{this.state.string}</p>
+          <button onClick={() => this.setState({
+            string: 'Text changed'
+          }) }>Change text</button>
+        </header>
+      </div>
+    )
+  }
+}
