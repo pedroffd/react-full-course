464cfed843f11f773d0699d193526978267982fc
pedroffd <pedro.alcarin@gmail.com>
2021-09-28 01:01:49 -0400

    prettier config added

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: false,
+}
